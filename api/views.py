ff07ccb09f8e4dee7983f10d90f70c2176ecaed5
Jason Christopher <jason.christopher@u.nus.edu>
2023-09-07 04:28:35 +0800

    Update files corresponding to API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+
+# Read data from S3
