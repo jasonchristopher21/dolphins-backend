1e872882837dcb6e0cbabda9245fcd2198f22838
Jason Christopher <jason.christopher@u.nus.edu>
2023-09-07 17:20:33 +0800

    Add Parsing Logic

--- api/views.py
+++ api/views.py
@@ -7,5 +7,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from api.serializers import (
+    S3FileSerializer,
+)
+class ParserView(APIView):
 
-# Read data from S3
+    def post(self, request, *args, **kwargs):
+        serializer_data = S3FileSerializer(data=request.data)
+        serializer_data.is_valid(raise_exception=True)
+        serializer_data.save()
+        return Response(serializer_data.data, status=status.HTTP_201_CREATED)
